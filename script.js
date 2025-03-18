f3e31bd8582cfdc684d53b6824270ba1cc50ca06
Shubhgpt456 <shubhgpt456@gmail.com>
2025-03-18 17:15:58 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+// Navbar Scroll Effect
+window.addEventListener("scroll", function() {
+    let navbar = document.getElementById("navbar");
+    if (window.scrollY > 50) {
+        navbar.classList.add("scrolled");
+    } else {
+        navbar.classList.remove("scrolled");
+    }
+});
+
+// Smooth Scrolling
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener("click", function(event) {
+        event.preventDefault();
+        document.querySelector(this.getAttribute("href")).scrollIntoView({
+            behavior: "smooth"
+        });
+    });
+});
